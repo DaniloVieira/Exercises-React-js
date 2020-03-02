f3b8029b54da89144d57557dd1191941fe39947f
Danilo <methodic4u@gmail.com>
2020-03-02 13:36:32 -0300

    assignment3 END

--- routing--assignment-problem/src/containers/Courses/Courses.js
+++ routing--assignment-problem/src/containers/Courses/Courses.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import './Courses.css';
+
+import Course from '../Course/Course';
 
 class Courses extends Component {
     state = {
@@ -11,6 +14,15 @@ class Courses extends Component {
         ]
     }
 
+    selectedHandler = (course) => {
+        console.log(course.id, course.title);
+        this.props.history.push({
+            pathname: '/courses/'+course.id,
+            search: '?title='+course.title,
+            state: { course: course }
+        });
+    }
+
     render () {
         return (
             <div>
@@ -18,10 +30,11 @@ class Courses extends Component {
                 <section className="Courses">
                     {
                         this.state.courses.map( course => {
-                            return <article className="Course" key={course.id}>{course.title}</article>;
+                            return <article className="Course" key={course.id} onClick={() => this.selectedHandler(course)}>{course.title}</article>;
                         } )
                     }
                 </section>
+                <Route  path={this.props.match.url + '/:id'} component={Course}/>
             </div>
         );
     }
